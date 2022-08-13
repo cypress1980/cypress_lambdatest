311d1aa9b163bd019cfe32954dbbadbb1c2e9e61
Kailash Pathak <kailash.pathak@3pillarglobal.com>
2022-08-13 20:35:46 +0530

    skip more tc

--- cypress/integration/lambdatest/ScrollAnd_ClickProduct.spec.js
+++ cypress/integration/lambdatest/ScrollAnd_ClickProduct.spec.js
@@ -1,15 +1,15 @@
 /// <reference types="cypress" />
-it("Open the Url", () => {
+it.skip("Open the Url", () => {
   cy.visit(
     "https://ecommerce-playground.lambdatest.io/index.php?route=account/login"
   );
 });
-it("Login into the application", () => {
+it.skip("Login into the application", () => {
   cy.get('[id="input-email"]').type("lambdatest@yopmail.com");
   cy.get('[id="input-password"]').type("lambdatest");
   cy.get('[type="submit"]').eq(0).click();
 });
-it("Click on Lambdatest Logo", () => {
+it.skip("Click on Lambdatest Logo", () => {
   cy.get('[title="Poco Electro"]').click();
   cy.wait(5000);
 });
